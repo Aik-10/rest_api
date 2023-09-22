b0ed992665e056ef8e1850fc5ff4a41266d69fb2
Lentokone <44865524+Aik-10@users.noreply.github.com>
2023-09-22 19:03:42 +0300

    fix: esbuild & cross-env added

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,52 +1,60 @@
-const { build } = require('esbuild');
-const { red, blueBright, yellowBright } = require('colorette');
+const esbuild = require("esbuild");
 
-const isWatchEnabled =
-    process.argv.findIndex((arg) => arg === '--mode=watch') !== -1;
+const IS_WATCH_MODE = process.env.IS_WATCH_MODE;
 
-function handleRebuild() {
-    return (error, result) => {
-        if (error) {
-            console.error(
-                `${yellowBright(new Date().toLocaleTimeString())} [${red(
-                    'ERROR',
-                )}] - Unable to rebuild project.`,
-                error,
-            );
-            return;
+const TARGET_ENTRIES = [
+    {
+        target: "es2020",
+        entryPoints: ["src/index.ts"],
+        platform: "node",
+        outfile: "./dist/server.js",
+    }
+];
+
+const buildBundle = async () => {
+    try {
+        const baseOptions = {
+            logLevel: "info",
+            bundle: true,
+            loader: {
+                ".ts": "ts",
+                ".js": "js",
+            },
+            minify: !IS_WATCH_MODE,
+            minifyWhitespace: !IS_WATCH_MODE,
+            charset: "utf8",
+            absWorkingDir: process.cwd(),
+        };
+
+        for (const targetOpts of TARGET_ENTRIES) {
+            const mergedOpts = { ...baseOptions, ...targetOpts };
+
+            const ctx = await esbuild.context({ ...mergedOpts });
+
+            if (IS_WATCH_MODE) {
+                console.log("starting watch");
+                await ctx.watch();
+            } else {
+                console.log(`[ESBUILD] Starting build ${JSON.stringify(targetOpts.entryPoints)}`);
+                const { errors, warnings } = ctx.rebuild();
+                if (errors?.length || warnings?.length) {
+                    if (errors?.length > 0) {
+                        console.error(`[ESBUILD] Bundle failed with ${errors.length} errors`);
+                        process.exit(1);
+                    }
+
+                    console.log(`[ESBUILD] Bundle failed with ${warnings.length} errors`);
+                }
+
+                console.log(`[ESBUILD] Builder finished successfully ${JSON.stringify(targetOpts.entryPoints)}`);
+                ctx.dispose();
+            }
         }
+    } catch (e) {
+        console.log("[ESBuild] Build failed with error");
+        console.error(e);
+        process.exit(1);
+    }
+};
 
-        console.log(
-            `${yellowBright(new Date().toLocaleTimeString())} [${blueBright(
-                'INFO',
-            )}] - Project rebuilt ${!result.warnings.length ? 'successfully' : `with ${red('warnings')}`
-            }.`,
-            result.warnings.length ? result.warnings : '',
-        );
-    };
-}
-
-build({
-    bundle: true,
-    entryPoints: ['src/index.ts'],
-    outfile: 'dist/server.js',
-    platform: 'node',
-    target: ['node16'],
-    format: 'cjs',
-    watch: isWatchEnabled && { onRebuild: handleRebuild() },
-    minify: !isWatchEnabled,
-    minifyWhitespace: !isWatchEnabled,
-})
-    .then(() =>
-        console.log(
-            `${yellowBright(new Date().toLocaleTimeString())} [${blueBright(
-                'INFO',
-            )}] - ${isWatchEnabled
-                ? 'Watching for changes...'
-                : 'Project built successfully!'
-            }`,
-        ),
-    )
-    .catch(() => {
-        console.error('Fatal error occurred during build process.')
-    });
+buildBundle().catch(() => process.exit(1));
